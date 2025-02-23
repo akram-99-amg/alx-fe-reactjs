1512591e041f15a65f01d844e44c589d3c35f11c
Akram <mazakram04@gmail.com>
2025-02-22 21:19:43 -0500

    update

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,9 +6,9 @@ const [count,setCount]=useState(0)
   return (
     <div>
       <p>Count : {count}</p>
-      <button onClick={(e)=>setCount(count +1)}> Increment</button>
-      <button onClick={(e)=>setCount(count -1)}> Decrement</button>
-      <button onClick={(e)=>setCount(0)}> Reset</button>
+      <button onClick={()=>setCount(count +1)}> Increment</button>
+      <button onClick={()=>setCount(count -1)}> Decrement</button>
+      <button onClick={()=>setCount(0)}> Reset</button>
       
     </div>
   )
