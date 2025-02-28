a5b7714b142d6a87a3c63054f32fa9ee91e6e788
Akram <mazakram04@gmail.com>
2025-02-28 14:11:23 -0500

    add task 1

---
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -0,0 +1,37 @@
+import { useRecipeStore } from '../store/recipeStore';
+import React, { useState } from 'react'
+
+const AddRecipeForm = () => {
+    const addRecipe =useRecipeStore(state=>state.addRecipe);
+    const[title,setTitel]=useState("");
+    const[description,setDescription]=useState("");
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        addRecipe({id:Date.now(), title, description});
+        setTitel("");
+        setDescription("");
+    }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text"
+      value={title}
+      onChange={(e)=>setTitel(e.target.value)}
+      placeholder="Title" />
+
+      <textarea 
+      value={description}
+       onChange={(e)=>setDescription(e.target.value)}
+       placeholder="Description"
+       />
+
+        <button type="submit">Add Recipe</button>
+
+       
+    </form>
+  );
+};
+
+export default AddRecipeForm
