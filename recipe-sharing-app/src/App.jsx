17d63e0e28617c5d280dc9c31d6d84ac388ece45
Akram <mazakram04@gmail.com>
2025-03-01 22:21:49 -0500

    add recipeList

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
+import RecipeList from "./components/RecipeList";
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
 
         <Route path="/" element={<AddRecipeForm />} />
+        <Route path="list/" element={<RecipeList />} />
         <Route path="details/" element={<RecipeDetails />} >
           <Route path=":recipeId" element={<RecipeDetails />} />
 
