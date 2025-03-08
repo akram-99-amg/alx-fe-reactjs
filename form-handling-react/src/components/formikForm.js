8ff401b5b6d1a02d30e1ba0058e3d8f48f7deb29
Akram <mazakram04@gmail.com>
2025-03-07 21:00:30 -0500

    add formikForm

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,56 +1,55 @@
-import React, { useState } from 'react'
+
 import * as Yup from "yup"
-import { ErrorMessage, useFormik } from "formik"
+import { Formik, Form, Field, ErrorMessage } from "formik"
 
-const RegistrationForm = () => {
-    const formik = useFormik({
-        initialValues: {
-            username: "",
-            email: "",
-            password: ""
-        },
-        validationSchema: Yup.object({
-            username: Yup.string().required("username required"),
-            email: Yup.string().email("invalid Email Format").required("Email required"),
-            password: Yup.string().required("Password required").min(6)
+const validationSchema= Yup.object({
+    username: Yup.string().required("username required"),
+    email: Yup.string().email("invalid Email Format").required("Email required"),
+    password: Yup.string().required("Password required").min(6)
 
 
-        }),
-        onSubmit: (values, { setErrors }) => {
-            if (values.username && values.email && values.password) {
-                alert("You signed in")
-            } else {
-                setErrors(ErrorMessage)
-            }
+})
 
+const FormikForm = () => (
+    <Formik 
+    initialValues= {{
+        username: "",
+        email: "",
+        password: ""
+    }}
+    validationSchema={validationSchema}
+    onSubmit={(values=>{
+        if (values.username && values.email && values.password) {
+            alert("You signed in")
+        } else {
+            setErrors(ErrorMessage)
         }
 
-    })
-
-
-
-    return (
-        <>
-
-            <form onSubmit={formik.handleSubmit}>
+    })}
+    > 
+    {()=>(
+        <Form>
                 <label htmlFor="username">Username</label>
-                <input type="text" name='username' onChange={formik.handleChange} />
-                {formik.touched.username && formik.errors.username && (<p>Enter a username</p>)}
-
+                <Field type="text" name='username' onChange={formik.handleChange} />
+                <ErrorMessage name="username" component="div" />
                 <label htmlFor="email">email</label>
-                <input type="email" name='email' onChange={formik.handleChange} />
-                {formik.touched.email && formik.errors.email && (<p>Enter a valid email</p>)}
+                <Field type="email" name='email' onChange={formik.handleChange} />
+                <ErrorMessage name="email" component="div" />
 
                 <label htmlFor="password">Password</label>
-                <input type="password" name='password' onChange={formik.handleChange} />
-                {formik.touched.password && formik.errors.password && (<p>Enter a password</p>)}
-
+                <Field type="password" name='password' onChange={formik.handleChange} />
+                <ErrorMessage name="password" component="div" />
 
                 <button type='submit'>sign in</button>
-            </form>
+            </Form>
 
-        </>
-    )
-}
+    )}
+    </Formik>
+   )
+   
 
-export default RegistrationForm
+
+
+
+
+export default FormikForm
