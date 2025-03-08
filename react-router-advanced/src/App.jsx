25d733eda7153d2cc91c513d7587db6843abb2c7
Akram <mazakram04@gmail.com>
2025-03-08 14:40:43 -0500

    add blog

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
 import Autenticated from './components/Autenticated';
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from './components/ProfileSettings';
+import UserProfile from './components/UserProfile';
+import BlogPost from './components/BlogPost';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Autenticated  />} />
+        <Route path="/" element={<Autenticated/>} />
         <Route path="/profile/*" element={<Profile />} />
-        <Route path="/profile/:details" element={<ProfileDetails />} />
-        <Route path="/profile/:settings" element={<ProfileSettings />} />
+        <Route path="/user/:userId" element={<UserProfile />} />
+        <Route path="/post/:postId" element={<BlogPost />} />
       </Routes>
     </Router>
   );
