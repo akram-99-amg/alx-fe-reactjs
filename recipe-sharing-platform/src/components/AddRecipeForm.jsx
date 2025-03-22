9e3ea91c5361e03da9d1450c8ef43cba2ca4003d
Akram <mazakram04@gmail.com>
2025-03-22 16:32:10 -0400

    fix bugs

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -4,25 +4,29 @@ const AddRecipeForm = () => {
     const [title, setTitle] = useState("")
     const [ingredients, setIngredients] = useState("")
     const [steps, setSteps] = useState("")
-    const [error, setError] = useState("")
+    const [error, setError] = useState({})
+const validate =()=>{
+    const newErrors ={};
+    if(!title) newErrors.title="title is required"
+    if(!ingredients) newErrors.ingredients="ingredients are required"
+    if(!steps) newErrors.steps="steps are required"
+    const ingredientList = ingredients.split(",").map(item => item.trim())
+        if (ingredientList.length < 2) 
+            newErrors.ingredients="Please include at least 2 ingredients"
+            setError(newErrors)
+            return Object.keys(newErrors).length === 0
+        
+}
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!title || !ingredients || !steps) {
-            setError("all fields are required")
-            return
-        }
-        const ingredientList = ingredients.split(",").map(item => item.trim())
-        if (ingredientList.length < 2) {
-            setError("Include at least 2 ingredients.")
-            return
-        }
+        if (!validate()) return
         setTitle("")
         setIngredients("")
         setSteps("")
         setError("")
-
-
-    }
+        }
+        
 
     return (
         <div className='max-w-lg mx-auto p-5 bg-gray-50 shadow-md rounded-lg mt-8'>
