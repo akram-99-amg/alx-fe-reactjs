30ad08db55122215f8aa2064d01351662ef95da1
Akram <mazakram04@gmail.com>
2025-03-31 07:36:02 -0400

    add q=

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -5,14 +5,14 @@ export const fetchUserData = async (query, location, minRepos) => {
       let searchQuery = `q=${query}`;
       if (location) searchQuery += `+location:${location}`;
       if (minRepos && !isNaN(minRepos)) searchQuery += `+repos:>${minRepos}`;
-  
-      const url = `https://api.github.com/search/users?${searchQuery}`;
+      
+      const url = `https://api.github.com/search/users?q=${searchQuery}`;
       
   
       const res = await axios.get(url);
-      
+      console.log("API Response:", res.data);
   
-      return res.data.items;
+      return res.data.items || [];
     } catch (error) {
       
       throw new Error("User not found");
